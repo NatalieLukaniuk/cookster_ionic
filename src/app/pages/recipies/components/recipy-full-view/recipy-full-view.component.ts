17585b9e6aa90d452a4d8ce1086bcffd7dbb3226
Natalie Lukaniuk <natalie.lukaniuk@gmail.com>
2023-01-09 16:34:59 +0200

    scrollable tabs: full recipy

--- src/app/pages/recipies/components/recipy-full-view/recipy-full-view.component.ts
+++ src/app/pages/recipies/components/recipy-full-view/recipy-full-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterContentInit, AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild, OnChanges, SimpleChanges } from '@angular/core';
 import { User } from 'src/app/models/auth.models';
 import { Recipy } from 'src/app/models/recipies.models';
 @Component({
@@ -6,12 +6,14 @@ import { Recipy } from 'src/app/models/recipies.models';
   templateUrl: './recipy-full-view.component.html',
   styleUrls: ['./recipy-full-view.component.scss'],
 })
-export class RecipyFullViewComponent implements OnInit {
+export class RecipyFullViewComponent implements OnChanges {
   @Input() recipy: Recipy | undefined | null;
   @Input() currentUser!: User | null;
 
   @Input() portions?: number;
   @Input() amountPerPortion?: number;
+
+  @ViewChild('header') header: ElementRef | undefined;
 
   tabs = [
     { value: 'ingredients', icon: '', name: 'Інгридієнти' },
@@ -24,8 +26,12 @@ export class RecipyFullViewComponent implements OnInit {
   selectedStepId = 0;
 
   constructor() {}
-
-  ngOnInit() {}
+  ngOnChanges(changes: SimpleChanges): void {
+    if(this.header){
+      let root = document.documentElement;
+      root.style.setProperty('--header-height', this.header.nativeElement.offsetHeight + 30 + "px");
+    }
+  }
 
   onTabChange(event: any) {
     this.currentTab = event.detail.value;
