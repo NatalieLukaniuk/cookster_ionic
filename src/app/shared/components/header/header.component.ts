d0fe4b758b9bd32831c2d83b004ed609bc8eb68a
Natalie Lukaniuk <natalie.lukaniuk@gmail.com>
2023-05-27 12:57:51 +0300

    revert

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,27 +1,14 @@
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
-import { Store, select } from '@ngrx/store';
-import { Subject, takeUntil } from 'rxjs';
-import { IAppState } from 'src/app/store/reducers';
-import { getPreviousRoute } from 'src/app/store/selectors/ui.selectors';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit, OnDestroy {
-  @Input() isShowBackButton = false;
-  previousRoute: string = '';
+export class HeaderComponent implements OnInit {
 
-  destroyed$ = new Subject<void>();
-  constructor(private store: Store<IAppState>) {}
-  ngOnDestroy(): void {
-    this.destroyed$.next();
-  }
+  constructor() { }
 
-  ngOnInit() {
-    this.store
-      .pipe(select(getPreviousRoute), takeUntil(this.destroyed$))
-      .subscribe((route) => (this.previousRoute = route));
-  }
+  ngOnInit() {}
+
 }
