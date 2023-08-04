e9943aceed9a891d36fed1bf33a6b2d0360324c0
Natalie Lukaniuk <natalie.lukaniuk@gmail.com>
2023-08-04 11:45:26 +0300

    user page details

--- src/app/pages/user/user.page.ts
+++ src/app/pages/user/user.page.ts
@@ -2,10 +2,11 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { IonModal } from '@ionic/angular';
 import { Store, select } from '@ngrx/store';
-import { tap } from 'rxjs';
-import { User } from 'src/app/models/auth.models';
+import { Observable, tap } from 'rxjs';
+import { FamilyMember, User } from 'src/app/models/auth.models';
+import { DataMappingService } from 'src/app/services/data-mapping.service';
 import { IAppState } from 'src/app/store/reducers';
-import { getCurrentUser } from 'src/app/store/selectors/user.selectors';
+import { getCurrentUser, getFamilyMembers } from 'src/app/store/selectors/user.selectors';
 
 @Component({
   selector: 'app-user',
@@ -29,7 +30,11 @@ export class UserPage implements OnInit {
 
   user$ = this.store.pipe(select(getCurrentUser), tap(user => this.currentUser = user));
 
-  constructor(private router: Router, private route: ActivatedRoute, private store: Store<IAppState>) { }
+  familyMembers$: Observable<FamilyMember[] | undefined> = this.store.pipe(select(getFamilyMembers));
+
+  sampleRecommendedPortion = 250;
+
+  constructor(private router: Router, private route: ActivatedRoute, private store: Store<IAppState>, private datamapping: DataMappingService) { }
 
   ngOnInit() {
   }
@@ -43,6 +48,10 @@ export class UserPage implements OnInit {
   goSettings(link: string) {
     this.dismissSettingsModal();
     this.router.navigate([link], {relativeTo: this.route});
+  }
+
+  getProductText(id: string): string {
+    return this.datamapping.getProductNameById(id);
   }
 
 }
