a4747bfbc61922d7306aa70157076adc9983124d
Natalie Lukaniuk <natalie.lukaniuk@gmail.com>
2023-09-04 09:31:46 +0300

    edit reminder functionality

--- src/app/pages/calendar/components/advance-preparation/advance-preparation.component.ts
+++ src/app/pages/calendar/components/advance-preparation/advance-preparation.component.ts
@@ -18,6 +18,8 @@ import { getCurrentUser } from 'src/app/store/selectors/user.selectors';
 import * as UserActions from './../../../../store/actions/user.actions';
 import * as moment from 'moment';
 import { Subject, takeUntil } from 'rxjs';
+import { ModalController } from '@ionic/angular';
+import { AddReminderModalComponent } from 'src/app/shared/components/dialogs/add-reminder-modal/add-reminder-modal.component';
 
 @Component({
   selector: 'app-advance-preparation',
@@ -33,7 +35,7 @@ export class AdvancePreparationComponent implements OnDestroy {
 
   destroy$ = new Subject<void>();
 
-  constructor(private store: Store, private dialog: DialogsService) {
+  constructor(private store: Store, private dialog: DialogsService, private modalCtrl: ModalController) {
     this.store.pipe(select(getCurrentUser), takeUntil(this.destroy$)).subscribe((res) => {
       if (res) {
         this.currentUser = res;
@@ -90,5 +92,45 @@ export class AdvancePreparationComponent implements OnDestroy {
 
   closeSlidingItem() {
     this.slidingContainer.close()
+  }
+
+  async onEditReminder(reminder: Reminder){
+    const modal = await this.modalCtrl.create({
+      component: AddReminderModalComponent,
+      breakpoints: [0.5, 0.75],
+      initialBreakpoint: 0.5,
+      componentProps: {
+        description: reminder.description,
+        dateTime: reminder.fullDate,
+        date: reminder.calendarDay
+      }
+    });
+    modal.present();
+
+    const { data, role } = await modal.onWillDismiss();
+
+    if (role === 'confirm') {
+
+      const updated: Reminder = {
+        description: data.description,
+        calendarDay: data.date,
+        fullDate: data.fullDate,
+        done: false
+      }
+      
+      if(this.currentUser){
+        const updatedUser = _.cloneDeep(this.currentUser);
+        if(updatedUser.savedPreps){
+          updatedUser.savedPreps = updatedUser.savedPreps.map(rem => {
+            if(rem.calendarDay === reminder.calendarDay && rem.description === reminder.description){
+              return updated
+            } else {
+              return rem;
+            }
+          })
+        } 
+        this.store.dispatch(new UserActions.UpdateUserAction(updatedUser, 'Нагадування оновлено'))
+      }
+    }
   }
 }
