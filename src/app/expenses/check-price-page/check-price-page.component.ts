cf31c71b73ec71833c28bc8dee7f6d19e1adbf5d
Natalie Lukaniuk <natalie.lukaniuk@gmail.com>
2023-10-28 10:34:59 +0300

    check price search functionality

--- src/app/expenses/check-price-page/check-price-page.component.ts
+++ src/app/expenses/check-price-page/check-price-page.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { select, Store } from '@ngrx/store';
-import { Observable, combineLatest, map, tap } from 'rxjs';
+import { BehaviorSubject, Observable, combineLatest, map, tap } from 'rxjs';
 import { IAppState } from 'src/app/store/reducers';
 import { getCurrentUser } from 'src/app/store/selectors/user.selectors';
 import { ExpenseItem } from '../expenses-models';
@@ -9,6 +9,7 @@ import * as _ from 'lodash';
 import { MeasuringUnit, MeasuringUnitOptions, MeasuringUnitText } from 'src/app/models/recipies.models';
 import { DataMappingService } from 'src/app/services/data-mapping.service';
 import { transformToGr } from 'src/app/pages/recipies/utils/recipy.utils';
+import { Filters } from 'src/app/models/filters.models';
 
 @Component({
   selector: 'app-check-price-page',
@@ -26,6 +27,8 @@ export class CheckPricePageComponent {
 
   filteredExpenses: ExpenseItem[] = [];
 
+  searchWord$ = new BehaviorSubject<string>('');
+
   selectedMeasuringUnit: MeasuringUnit = MeasuringUnit.gr;
   selectedQuantity = 100;
 
@@ -37,9 +40,22 @@ export class CheckPricePageComponent {
 
   expenseItemsToDisplay$ = combineLatest([
     this.expenses$,
-    this.filtersService.getFilters
+    this.filtersService.getFilters,
+    this.searchWord$
   ]).pipe(
     map(res => _.cloneDeep(res)),
+    map(res => {
+      const data: ExpenseItem[] = res[0];
+      const searchword = res[2];
+      if (searchword.length) {
+        const filteredData: ExpenseItem[] = data.filter(item => item.title.toLowerCase().includes(searchword.toLowerCase()));
+        const toReturn: [ExpenseItem[], Filters] = [filteredData, res[1]];
+        return toReturn
+      } else {
+        const toReturn: [ExpenseItem[], Filters] = [data, res[1]];
+        return toReturn
+      }
+    }),
     map((res) => this.filtersService.applyFiltersToExpenses(res[0], res[1])),
     tap(res => this.filteredExpenses = res),
     tap(() => this.getAveragePrice())
@@ -75,33 +91,42 @@ export class CheckPricePageComponent {
     return pricePerSelected < this.averagePrice
   }
 
-  getIsAboveAverage(expense: ExpenseItem){
+  getIsAboveAverage(expense: ExpenseItem) {
     const pricePerSelected = this.getInGramsPerSelectQuanityAndMeasuringUnit(expense) * (expense.costPerHundredGrInHRN / 100)
     return pricePerSelected > this.averagePrice
   }
 
-  getLowestPrice(){
+  getLowestPrice() {
     let lowest = 100000;
     this.filteredExpenses.forEach(item => {
-      if(item.costPerHundredGrInHRN < lowest){
+      if (item.costPerHundredGrInHRN < lowest) {
         lowest = item.costPerHundredGrInHRN
       }
     })
     return lowest
   }
 
-  getHighestPrice(){
+  getHighestPrice() {
     let highest = 0;
     this.filteredExpenses.forEach(item => {
-      if(item.costPerHundredGrInHRN > highest){
+      if (item.costPerHundredGrInHRN > highest) {
         highest = item.costPerHundredGrInHRN
       }
     })
     return highest
   }
 
-  getColor(expense: ExpenseItem){
-    return expense.costPerHundredGrInHRN === this.lowestPrice? 'success' : expense.costPerHundredGrInHRN === this.highestPrice? 'warning' : this.getIsBelowAverage(expense)? 'light' : this.getIsAboveAverage(expense)? 'medium' : 'secondary'
+  getColor(expense: ExpenseItem) {
+    return expense.costPerHundredGrInHRN === this.lowestPrice ? 'success' : expense.costPerHundredGrInHRN === this.highestPrice ? 'warning' : this.getIsBelowAverage(expense) ? 'light' : this.getIsAboveAverage(expense) ? 'medium' : 'secondary'
+  }
+
+  onSearch(event: any) {
+    const searchword = event.detail.value;
+    this.searchWord$.next(searchword);
+  }
+
+  clear() {
+    this.searchWord$.next('');
   }
 
 }
