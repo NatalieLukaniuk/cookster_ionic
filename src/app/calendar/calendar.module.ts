37f3e0823d69a3f33cb72f3012ea399ed12aa4aa
Natalie Lukaniuk <natalie.lukaniuk@gmail.com>
2024-09-19 16:34:44 +0300

    add day selector

--- src/app/calendar/calendar.module.ts
+++ src/app/calendar/calendar.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CalendarComponent } from './calendar-smart-wrapper/calendar.component';
 import { CalendarTimelineDayComponent } from './calendar-timeline-day/calendar-timeline-day.component';
+import { IonicModule } from '@ionic/angular';
 
 
 
 @NgModule({
   declarations: [CalendarComponent, CalendarTimelineDayComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    IonicModule,
   ],
   exports: [CalendarComponent]
 })
