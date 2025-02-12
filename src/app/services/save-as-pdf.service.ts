b4806479b8890a7f9e61faa9b3e0c9c306b6ce90
Natalie Lukaniuk <natalie.lukaniuk@gmail.com>
2025-02-12 11:57:04 +0200

    save as pdf logic and libs

---
+++ src/app/services/save-as-pdf.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import * as jspdf from 'jspdf';
+import html2canvas from 'html2canvas';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SaveAsPdfService {
+
+  constructor() { }
+
+  public captureScreen() {
+    var data = document.getElementById('contentToConvert');  //Id of the table
+    if (data) {
+      html2canvas(data).then(canvas => {
+        // Few necessary setting options  
+        let imgWidth = 208;
+        let pageHeight = 295;
+        let imgHeight = canvas.height * imgWidth / canvas.width;
+        let heightLeft = imgHeight;
+
+        const contentDataURL = canvas.toDataURL('image/png')
+        let pdf = new jspdf.jsPDF('p', 'mm', 'a4'); // A4 size page of PDF  
+        let position = 0;
+        pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)
+        pdf.save('MYPdf.pdf'); // Generated PDF   
+      });
+    }
+
+  }
+}
