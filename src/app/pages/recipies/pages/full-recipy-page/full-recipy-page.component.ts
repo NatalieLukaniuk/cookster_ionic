5d07686b8e78294a27e37d243b35412a3822c0d7
Natalie Lukaniuk <natalie.lukaniuk@gmail.com>
2025-05-03 14:36:38 +0300

    change title for recipy page

--- src/app/pages/recipies/pages/full-recipy-page/full-recipy-page.component.ts
+++ src/app/pages/recipies/pages/full-recipy-page/full-recipy-page.component.ts
@@ -3,19 +3,20 @@ import { SetIsLoadingFalseAction } from './../../../../store/actions/ui.actions'
 import { filter, map, tap } from 'rxjs/operators';
 import { getAllRecipies } from 'src/app/store/selectors/recipies.selectors';
 import { Store, select } from '@ngrx/store';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { IAppState } from 'src/app/store/reducers';
 import { SetIsLoadingAction } from 'src/app/store/actions/ui.actions';
 import * as _ from 'lodash';
 import { Router } from '@angular/router';
 import { combineLatest } from 'rxjs';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-full-recipy-page',
   templateUrl: './full-recipy-page.component.html',
   styleUrls: ['./full-recipy-page.component.scss'],
 })
-export class FullRecipyPageComponent implements OnInit {
+export class FullRecipyPageComponent implements OnInit, OnDestroy {
   recipyId: string;
 
   recipy$ = this.store.pipe(
@@ -28,6 +29,8 @@ export class FullRecipyPageComponent implements OnInit {
         let updatedRecipy = _.cloneDeep(recipy);
         updatedRecipy.ingrediends.sort((a, b) => b.amount - a.amount);
         this.store.dispatch(new SetIsLoadingFalseAction());
+        this.titleService.setTitle(recipy.name)
+        console.log('updated')
         return updatedRecipy;
       } else return recipy;
     })
@@ -38,9 +41,12 @@ export class FullRecipyPageComponent implements OnInit {
     filter((res) => !!res[0] && !!res[1]),
     map((res) => res[0]?.email === res[1]?.author)
   );
-  constructor(private store: Store<IAppState>, private router: Router) {
+  constructor(private store: Store<IAppState>, private router: Router, private titleService: Title) {
     const path = window.location.pathname.split('/');
     this.recipyId = path[path.length - 1];
+  }
+  ngOnDestroy(): void {
+    this.titleService.setTitle('Cookster')
   }
 
   ngOnInit() {}
