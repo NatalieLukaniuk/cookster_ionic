dcda0fd5e5f3f7609cbbab48ff4a7d7ecefdd3d8
Natalie Lukaniuk <natalie.lukaniuk@gmail.com>
2025-05-20 13:27:24 +0300

    select product to edit

--- src/app/pages/admin/pages/update-products/update-products.component.ts
+++ src/app/pages/admin/pages/update-products/update-products.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { map, Observable } from 'rxjs';
+import { Product } from 'src/app/models/recipies.models';
+import { IAppState } from 'src/app/store/reducers';
+import { getAllProducts } from 'src/app/store/selectors/recipies.selectors';
 
 @Component({
   selector: 'app-update-products',
@@ -6,8 +11,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./update-products.component.scss']
 })
 export class UpdateProductsComponent implements OnInit {
+products: Product[] = [];
+  products$: Observable<Product[]> = this.store.pipe(
+    select(getAllProducts),
+    map((res) => {
+      if (res) {
+        let products = res.map((i) => i);
+        products.sort((a, b) => a.name.localeCompare(b.name));
+        this.products = products;
+        return products;
+      } else return [];
+    })
+  );
+  constructor(private store: Store<IAppState>,) { }
 
-  constructor() { }
+  onProductSelected(selected: any){}
 
   ngOnInit() {
   }
