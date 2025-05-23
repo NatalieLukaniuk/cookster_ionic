d36a1bfda11f95b82373fd8f21bdd7a0564c9c65
Natalie Lukaniuk <natalie.lukaniuk@gmail.com>
2025-05-23 13:06:28 +0300

    product update form

--- src/app/pages/admin/pages/update-products/update-products.component.ts
+++ src/app/pages/admin/pages/update-products/update-products.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UntypedFormControl, UntypedFormGroup, Validators } from '@angular/forms';
 import { select, Store } from '@ngrx/store';
 import { map, Observable } from 'rxjs';
 import { Product } from 'src/app/models/recipies.models';
@@ -11,7 +12,12 @@ import { getAllProducts } from 'src/app/store/selectors/recipies.selectors';
   styleUrls: ['./update-products.component.scss']
 })
 export class UpdateProductsComponent implements OnInit {
-products: Product[] = [];
+
+  selectedProduct: Product | null = null;
+
+  productForm!: UntypedFormGroup;
+
+  products: Product[] = [];
   products$: Observable<Product[]> = this.store.pipe(
     select(getAllProducts),
     map((res) => {
@@ -23,11 +29,48 @@ products: Product[] = [];
       } else return [];
     })
   );
+
+  nochanges = true;
+  
   constructor(private store: Store<IAppState>,) { }
 
-  onProductSelected(selected: any){}
+  onProductSelected(selected: Product) {
+    this.selectedProduct = selected;
+    console.log(this.selectedProduct)
+    this.initForm(this.selectedProduct)
+  }
 
   ngOnInit() {
+  }
+
+  initForm(product: Product) {
+    this.productForm = new UntypedFormGroup({
+      calories: new UntypedFormControl(product.calories, [
+        Validators.required,
+      ]),
+      defaultUnit: new UntypedFormControl(product.defaultUnit, [
+        Validators.required,
+      ]),
+      density: new UntypedFormControl(product.density, [
+        Validators.required,
+      ]),
+      grInOneItem: new UntypedFormControl(product.grInOneItem, [
+        Validators.required,
+      ]),
+      name: new UntypedFormControl(product.name, [
+        Validators.required,
+      ]),
+      type: new UntypedFormControl(product.type, [
+        Validators.required,
+      ]),
+      sizeChangeCoef: new UntypedFormControl(product.sizeChangeCoef, [
+        Validators.required,
+      ]),
+    });
+  }
+
+  submit(){
+
   }
 
 }
